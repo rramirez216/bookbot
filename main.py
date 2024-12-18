bd7bfe0da86a5b48da32acb93c52e9ca75507032
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-17 20:47:35 -0800

    creates function that counts the characters in the txt file and stores them in a dictionary

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def main():
+    character_count = {}
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         split_string = file_contents.split()
-        print(len(split_string))
 
-    print(split_string[2])
+    for i in range(0, len(split_string)):
+        new_str_arr = list(split_string[i].lower())
+        for i in range(0, len(new_str_arr)):
+            letter = new_str_arr[i]
+            if letter in character_count:
+                character_count[letter] = character_count[letter] + 1
+            else:
+                character_count[letter] = 1
+
+    print(character_count)
 
 
 main()
